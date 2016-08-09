07e5aa5ef6bf849f139db583f81590cd32339765
Steve Good <sgood@lanctr.com>
2016-08-08 23:11:42 -0500

    Initial commit

---
+++ src/test/groovy/tv/extrememoderation/UserApplicationTests.groovy
@@ -0,0 +1,16 @@
+package tv.extrememoderation
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
